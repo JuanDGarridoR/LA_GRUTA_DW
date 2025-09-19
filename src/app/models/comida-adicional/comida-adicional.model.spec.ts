d0790bc8693aa05c11d98aac0af2d0911903757a
JuanDGarridoR <juandgarridor@gmail.com>
2025-09-19 14:40:58 -0500

    Models completos

---
+++ src/app/models/comida-adicional/comida-adicional.model.spec.ts
@@ -0,0 +1,7 @@
+import { ComidaAdicional } from './comida-adicional.model';
+
+describe('ComidaAdicional', () => {
+  it('should create an instance', () => {
+    expect(new ComidaAdicional()).toBeTruthy();
+  });
+});
---
+++ src/app/models/comida-adicional/comida-adicional.model.ts
@@ -0,0 +1,5 @@
+export interface ComidaAdicional {
+  id?: number;          // id de la relación
+  comidaId: number;     // id de la comida
+  adicionalId: number;  // id del adicional
+}
